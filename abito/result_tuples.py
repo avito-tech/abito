5fb0ca4dd9a3df5f08e7c9f7a98cf75994378150
Danila Lenkov <dlenkov@avito.ru>
2019-06-07 14:05:32 +0300

    result_tuples bugfix

--- abito/result_tuples.py
+++ abito/result_tuples.py
@@ -16,14 +16,14 @@ MannWhitneyUTestResult = namedtuple('MannWhitneyUTestResult', [
     'z_statistic',
     'p_value',
 ])
-MannWhitneyUTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+MannWhitneyUTestResult.__new__.__defaults__ = (nan,) * len(MannWhitneyUTestResult._fields)
 
 
 ShapiroTestResult = namedtuple('ShapiroTestResult', [
     'statistic',
     'p_value',
 ])
-ShapiroTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+ShapiroTestResult.__new__.__defaults__ = (nan,) * len(ShapiroTestResult._fields)
 
 
 MedianTestResult = namedtuple('MedianTestResult', [
@@ -31,21 +31,21 @@ MedianTestResult = namedtuple('MedianTestResult', [
     'p_value',
     'grand_median',
 ])
-MedianTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+MedianTestResult.__new__.__defaults__ = (nan,) * len(MedianTestResult._fields)
 
 
 LeveneTestResult = namedtuple('LeveneTestResult', [
     'statistic',
     'p_value',
 ])
-LeveneTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+LeveneTestResult.__new__.__defaults__ = (nan,) * len(LeveneTestResult._fields)
 
 
 MoodTestResult = namedtuple('MoodTestResult', [
     'statistic',
     'p_value',
 ])
-MoodTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+MoodTestResult.__new__.__defaults__ = (nan,) * len(MoodTestResult._fields)
 
 
 BootstrapTestResult = namedtuple('BootstrapTestResult', [
@@ -54,4 +54,4 @@ BootstrapTestResult = namedtuple('BootstrapTestResult', [
     'mean_diff',
     'mean_diff_std'
 ])
-BootstrapTestResult.__new__.__defaults__ = (nan,) * len(TTestResult._fields)
+BootstrapTestResult.__new__.__defaults__ = (nan,) * len(BootstrapTestResult._fields)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='abito',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     description='Package for hypothesis testing in A/B-experiments',
     long_description=README,
